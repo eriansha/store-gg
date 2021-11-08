ef2c84d7fb299cac4167b504aa0e87dce6e2b429
Ivan Putra Eriansya <eriansha.van@gmail.com>
2021-11-08 21:59:45 +0700

    feature: sign up photo page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import '../styles/checkout.css'
 import '../styles/complete-checkout.css'
 import '../styles/sign-in.css'
 import '../styles/sign-up.css'
+import '../styles/sign-up-photo.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
